a253bb922c44ad592b1b5c191b1f54c53b538bff
suryansh.cometchat <suryansh.bisen@cometchat.com>
2025-04-23 19:17:04 +0530

    v4.0.56

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CometChatSDK",
-            url: "https://library.cometchat.io/ios/v4.0/xcode15/CometChatSDK_4_0_55.xcframework.zip",
-            checksum: "dc22a5e61c8fdcf4214b6d00747678d8b3cfc17dc48935221751c8b168666f33"
+            url: "https://library.cometchat.io/ios/v4.0/xcode15/CometChatSDK_4_0_56.xcframework.zip",
+            checksum: "bbc76cbb0a3c3b0c6dc1216c30a51ed9b47c714174b594e9f250297b5ab80727"
         ),
         .binaryTarget(
             name: "CometChatStarscream",
-            url: "https://library.cometchat.io/ios/v4.0/xcode15/CometChatStarscream_1_0_1.xcframework.zip",
-            checksum: "13a5968a10051dacfbc1fae4be585db59be4ef92e7b7ec33017d95d87a30bdd6"
+            url: "https://library.cometchat.io/ios/v4.0/xcode15/CometChatStarscream_1_0_2.xcframework.zip",
+            checksum: "94fef289d5b952bb06465e7631be83b40594072931a580a9e8ba86258b9f0ec5"
         )
     ]
 )
