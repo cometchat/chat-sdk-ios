efe05ae244782e11abe542d6e78e6a768bd570b4
Dawinder Kaur <dawinder.kaur@cometchat.com>
2025-11-24 20:24:10 +0530

    v4.0.72

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CometChatSDK",
-            url: "https://library.cometchat.io/ios/v4.0/xcode16/CometChatSDK_4_0_71.xcframework.zip",
-            checksum: "0053c9ee6ee82a4ec9567286a57f5b49ec596a4fe89e763fc4c7c772bce7ed3d"
+            url: "https://library.cometchat.io/ios/v4.0/xcode16/CometChatSDK_4_0_72.xcframework.zip",
+            checksum: "e4fb566d6b147e84e435106fff944c3be5638d027e4692029c84eab34a1253a8"
         ),
         .binaryTarget(
             name: "CometChatStarscream",
